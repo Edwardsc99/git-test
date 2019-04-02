e5fa36631d92648bf238e7aefd4cc93de803c0ca
Christian Edwards (s5109267) <s5109267@mac38566.mac.bournemouth.ac.uk>
2019-04-02 11:36:20 +0100

    removed comments

--- git test/ViewController.swift
+++ git test/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  git test
-//
-//  Created by Christian Edwards (s5109267) on 02/04/2019.
-//  Copyright © 2019 Christian Edwards (s5109267). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
